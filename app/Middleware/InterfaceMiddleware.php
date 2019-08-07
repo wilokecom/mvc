6e09f7595f22829335585eb6a0b1a9c0dd0d75a4
doduc <duccuong070294.uneti@gmail.com>
2019-08-07 17:12:59 +0700

    File middleware

--- app/Middleware/InterfaceMiddleware.php
+++ app/Middleware/InterfaceMiddleware.php
@@ -5,3 +5,18 @@
  * Date: 31/07/2019
  * Time: 11:24 SA
  */
+namespace MVC\Middleware;
+
+/**
+ * Interface InterfaceMiddleware
+ * @package MVC\Middleware
+ */
+interface InterfaceMiddleware
+{
+    /**
+     * InterfaceMiddleware constructor.
+     * @param array $aData
+     */
+    public function __construct($aData = []);
+    public function handle();
+}
